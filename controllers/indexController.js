e39825f8beb0ee69feeb5c9633ae4efcfd2f481e
lthphu <pminimalism2011@gmail.com>
2023-06-04 12:00:57 +0700

    small update UI

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -4,31 +4,32 @@
 const controller = {}
 
 // Show homepage
-controller.showHomePage = (req, res) =>{
-    res.render('index')
+controller.showHomePage = (req, res) => {
+  res.render('index')
 }
 
-controller.showPage = (req, res, next) =>{
-    const pages = [
-        "post-list-category",
-        "admin-categories"  ,
-        "post-list-tag",
-        "profile",
-        "admin",
-        "admin-categories",
-        "admin-posts",
-        "admin-tags",
-        "admin-users",
-        "signin",
-        "signup",
-        "writer-create",
-        "article-review",
-        "publish-article"
-      ];
-      if (pages.includes(req.params.page)) {
-        return res.render(req.params.page);
-      }
-      next();
+controller.showPage = (req, res, next) => {
+  const pages = [
+    "post-list-category",
+    "admin-categories",
+    "post-list-tag",
+    "profile",
+    "admin",
+    "admin-categories",
+    "admin-posts",
+    "admin-tags",
+    "admin-users",
+    "signin",
+    "signup",
+    "writer-create",
+    "article-review",
+    "publish-article",
+    "forgot-password"
+  ];
+  if (pages.includes(req.params.page)) {
+    return res.render(req.params.page);
+  }
+  next();
 }
 
 module.exports = controller
