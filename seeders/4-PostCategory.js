5ab9c55ad7d01789203436ff58883b7ff5c89426
nlduy0310 <nlduy0310@gmail.com>
2023-06-08 00:09:07 +0700

    seeders for post & post-categories; added script to reset id sequence

--- seeders/4-PostCategory.js
+++ seeders/4-PostCategory.js
@@ -3,71 +3,18 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    let items = [
-      {
-        postId: 1,
-        categoryId: 1,
-      },
-      {
-        postId: 1,
-        categoryId: 8,
-      },
-      {
-        postId: 2,
-        categoryId: 1,
-      },
-      {
-        postId: 2,
-        categoryId: 8,
-      },
-      {
-        postId: 3,
-        categoryId: 1,
-      },
-      {
-        postId: 3,
-        categoryId: 8,
-      },
-      {
-        postId: 4,
-        categoryId: 1,
-      },
-      {
-        postId: 4,
-        categoryId: 8,
-      },
-      {
-        postId: 5,
-        categoryId: 2,
-      },
-      {
-        postId: 5,
-        categoryId: 17,
-      },
-      {
-        postId: 6,
-        categoryId: 2,
-      },
-      {
-        postId: 6,
-        categoryId: 17,
-      },
-      {
-        postId: 7,
-        categoryId: 2,
-      },
-      {
-        postId: 7,
-        categoryId: 17,
-      },
-    ];
-    items = items.filter(
-      (value, index, self) =>
-        index ===
-        self.findIndex(
-          (t) => t.postId === value.postId && t.categoryId === value.categoryId
-        )
-    );
+    let data = require('./sample_data/posts.json');
+    let items = [];
+    data.forEach(datum => {
+      let post_idx = datum.idx;
+      datum.cat_idxs.forEach(cat_idx => {
+        items.push({
+          postId: post_idx,
+          categoryId: cat_idx
+        });
+      });
+    });
+
     items.forEach((item) => {
       item.createdAt = Sequelize.literal("NOW()");
       item.updatedAt = Sequelize.literal("NOW()");
---
+++ seeders/sql_scripts/reset_id_seqs.sql
@@ -0,0 +1,13 @@
+ALTER SEQUENCE public."ApprovedPosts_id_seq" RESTART WITH 1;
+ALTER SEQUENCE public."Categories_id_seq" RESTART WITH 1;
+ALTER SEQUENCE public."CommentStatuses_id_seq" RESTART WITH 1;
+ALTER SEQUENCE public."PostComments_id_seq" RESTART WITH 1;
+ALTER SEQUENCE public."PostImages_id_seq" RESTART WITH 1;
+ALTER SEQUENCE public."Posts_id_seq" RESTART WITH 1;
+ALTER SEQUENCE public."PostStatistics_id_seq" RESTART WITH 1;
+ALTER SEQUENCE public."PostStatuses_id_seq" RESTART WITH 1;
+ALTER SEQUENCE public."PremiumDetails_id_seq" RESTART WITH 1;
+ALTER SEQUENCE public."RejectedPosts_id_seq" RESTART WITH 1;
+ALTER SEQUENCE public."Tags_id_seq" RESTART WITH 1;
+ALTER SEQUENCE public."Users_id_seq" RESTART WITH 1;
+ALTER SEQUENCE public."UserTypes_id_seq" RESTART WITH 1;
